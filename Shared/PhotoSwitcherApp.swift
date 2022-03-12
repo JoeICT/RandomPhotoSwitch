ce70c717df6f1189efc2b39e906ab12cd7f14938
Joe Hunter <jrhunt1706@gmail.com>
2022-03-12 12:06:57 -0600

    Initial Commit

---
+++ Shared/PhotoSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwitcherApp.swift
+//  Shared
+//
+//  Created by user217937 on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
